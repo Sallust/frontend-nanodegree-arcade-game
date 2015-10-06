89e1d3285812b72a5f5ed0e690f3dfb5e0164958
Sallust <daniyom@gmail.com>
2015-10-06 19:28:42 -0400

    feat: add basic elements
    create Player class
    set initial x and y values (initial placement) randomly to see where rendered
    create handleInput method on player obj with arbitrary movement of 15 pixels

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
 // Enemies our player must avoid
 var Enemy = function() {
+
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = 0;
+    this.y = 100;
+
+    this.speed = 3;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +20,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,11 +31,45 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(){
+    this.x = 150;
+    this.y = 200;
+    this.sprite = 'images/char-boy.png';
 
+}
+
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+
+};
+
+// Draw the enemy on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(input){
+    if (input == 'left')
+        this.x -= 15;
+    if (input == 'right')
+        this.x +=15;
+    if (input == 'up')
+        this.y -=15;
+    if (input == 'down')
+        this.y +=15;
+
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [new Enemy()];
+
+var player = new Player();
+
+
 
 
 
